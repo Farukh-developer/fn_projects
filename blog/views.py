c9f56f89209b36be9badafd83ec7679dd7a50ad4
farukh <farukh@farukhs-MacBook-Pro.local>
2024-05-05 23:30:10 -0700

    ORM

--- blog/views.py
+++ blog/views.py
@@ -2,8 +2,13 @@ from django.shortcuts import render
 
 
 from.models import Country
-
 def country(request):
+    count=Country.objects.all()
+    return render(request, 'country.html', context={"count":count})
+
+
+
+def read_one_student(request):
     count=Country.objects.all()
     return render(request, 'country.html', context={"count":count})
 
@@ -30,7 +35,30 @@ def food(request):
 
 
 from.models import Laptop
-
+ 
 def laptop(request):
     computer=Laptop.objects.all()
     return render(request, 'laptop.html', context={"computer":computer})
+
+# add (Read)
+
+def read_one_comapany(request, id):
+    comp=Company.objects.get(id=id)
+    return render(request, 'read_location.html', context={"comp":comp})
+
+def read_country(request, id):
+    count=Country.objects.get(id=id)
+    return render(request, 'read_country.html', context={"count":count})
+
+def read_food(request, id):
+    meal=Food.objects.get(id=id)
+    return render(request, 'read_food.html', context={"meal":meal} )
+
+def read_latop(request, id):
+    computer=Laptop.objects.get(id=id)
+    return render(request, 'read_laptop.html', context={"computer":computer}) 
+
+
+def read_phone(request, id):
+    mobile=Phone.objects.get(id=id)
+    return render(request, 'read_phone.html', context={"mobile":mobile}) 
